b220b793b7626d1caf363d0366f7ffabdc9ba222
Letícia Cotta <leticiacotta@gmail.com>
2022-08-02 03:06:57 -0300

    add dev

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-
 import StrangerThings from './components/StrangerThings';
+
+require('dotenv').config();
 
 function App() {
   return (
     <div className="App">
       <StrangerThings />
+      <span> Em desenvolvimento </span>
     </div>
   );
 }
